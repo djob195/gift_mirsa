58b0ad693ebec77c7c60aed0421e374c40a46b7e
diego orellana <djorellanab@outlook.com>
2023-10-13 22:41:42 -0600

    change

---
+++ src/components/cake/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BirthdayCake = () => {
+  return (
+    <div className="birthday-container">
+    <h1 className="birthday-text">Happy Birthday!</h1>
+    <div className="balloons">
+      <div className="balloon balloon1"></div>
+      <div className="balloon balloon2"></div>
+      <div className="balloon balloon3"></div>
+    </div>
+  </div>
+  );
+};
+
+export default BirthdayCake;
