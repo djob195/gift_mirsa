b96b05d0e2c04fc48bf122c525f8f0d7435891e5
diego orellana <djorellanab@outlook.com>
2023-10-13 23:32:15 -0600

    change

--- src/components/cake/index.tsx
+++ src/components/cake/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const BirthdayCake = () => {
   return (
     <div className="birthday-container">
-    <h1 className="birthday-text">Happy Birthday!</h1>
+    <h1 className="birthday-text">Happy Birthday Gabriella!</h1>
+    <h2 className="birthday-text">Working on your Gift...!</h2>
     <div className="balloons">
       <div className="balloon balloon1"></div>
       <div className="balloon balloon2"></div>
